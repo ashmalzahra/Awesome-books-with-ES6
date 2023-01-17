36f04b951bf3aaf24fcabfa0c2ae34278d0ea1d0
ashmalzahra <ashmal.zahraa@gmail.com>
2023-01-17 21:31:48 +0500

    Create and export website navigation

---
+++ modules/pageNavigation.js
@@ -0,0 +1,36 @@
+export const changeSection = () => {
+window.addEventListener('load', () => {
+    const navBtn = document.querySelectorAll('nav > a');
+    const booksSection = document.querySelector('.all-books');
+    const formSection = document.querySelector('#form');
+    const contactSection = document.querySelector('#contact');
+    const timeElement = document.querySelector('span');
+  
+    setInterval(() => {
+      currentTime(timeElement);
+    }, 1000);
+  
+    navBtn.forEach((btn) => {
+      btn.addEventListener('click', () => {
+        const i = document.querySelector('.active');
+        if (i != null) {
+          i.classList.remove('active');
+        }
+        btn.className += 'active';
+        if (btn.getAttribute('href') === '#books') {
+          booksSection.classList.remove('hidden');
+          formSection.classList.add('hidden');
+          contactSection.classList.add('hidden');
+        } else if (btn.getAttribute('href') === '#form') {
+          formSection.classList.remove('hidden');
+          booksSection.classList.add('hidden');
+          contactSection.classList.add('hidden');
+        } else {
+          contactSection.classList.remove('hidden');
+          formSection.classList.add('hidden');
+          booksSection.classList.add('hidden');
+        }
+      });
+    });
+  });
+};
